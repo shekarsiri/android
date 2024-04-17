233fc6db91332bcd1a13530c55f7f6a81f21ce14
Shekar Siri <sshekarsiri@gmail.com>
2024-04-17 14:24:03 +0200

    publish changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,23 @@
-// Top-level build file where you can add configuration options common to all sub-projects/modules.
-plugins {
-    id("com.android.application") version "8.2.2" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
-}
-
 buildscript {
-    val kotlinVersion = "1.9.22"
-
+    ext.kotlin_version = "1.6.10"
     repositories {
         google()
         mavenCentral()
-        maven {
-            url = uri("https://jitpack.io")
-        }
-
-        dependencies {
-            classpath("com.android.tools.build:gradle:8.2.2")
-            classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlinVersion")
-        }
     }
+    dependencies {
+        classpath 'com.android.tools.build:gradle:4.2.2'
+        classpath "org.jetbrains.kotlin:kotlin-gradle-plugin:$kotlin_version"
+    }
+}
+
+allprojects {
+    repositories {
+        google()
+        maven { url "https://jitpack.io" }
+        mavenCentral()
+    }
+}
+
+task clean(type: Delete) {
+    delete rootProject.buildDir
 }
